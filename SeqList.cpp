2f3d00372451e3b20d6e3ac823fc87db5ad1ebdb
hekun1997 <1576053065@qq.com>
2020-07-21 17:40:10 +0800

    Update SeqList.cpp

--- SeqList.cpp
+++ SeqList.cpp
@@ -174,10 +174,73 @@ bool mergeList(SeqList a, SeqList b, SeqList &c){
     c.length = k;
     return true;
 }
-//8. (a, )
+//8. (a1, a2 ,a3 ......am, b1, b2, .....bn) -> (b1, b2, ....bn, a1, a2, ....an)
+typedef int DataType;
+void Reverse(DataType list[], int left, int right,int arraySize){
+    if (left >= right || right > arraySize)
+    {
+        return;
+    }
+    int mid = (left + right) / 2;
+    for (int i = 0; i < mid - left; i++){
+        DataType temp = list[left + i];
+        list[left + i] = list[right - i];
+        list[right - i] = temp;
+    }
+}
+
+void Exchange(DataType list[], int m, int n, int arraySize){
+    Reverse(list, 0, m + n - 1, arraySize);
+    Reverse(list, 0, n - 1, arraySize);
+    Reverse(list, n, m + n - 1, arraySize);
+}
+
+//9.最短的时间里找到x的位置，找到x，与后继元素交换，找不到则插入
+void ExchangeXOrInsertX(DataType list[], DataType x, int n){
+    int low = 0, high = n - 1, mid;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (list[mid] == x)
+            break;
+        else if (list[mid] > x)
+            high = mid -1;
+        else if (list[mid] < x)
+            low = mid +1;
+    }
+    if (list[mid] == x && mid != n-1)
+    {
+        DataType temp = list[mid];
+        list[mid] = list[mid + 1];
+        list[mid + 1] = temp;
+    }
+    if (low > high)
+    {
+        mid = 0;
+        for (; mid < high && list[mid] < x; mid++);
+        for (int i = high; i > mid; i --){
+            list[i + 1] = list[i];
+        }
+        list[mid] = x;
+    }
+}
+//10. (a0, a1,...,an-1) -> 左移p个位置->(ap, .... an-1,a0,a1,...ap-1)
+void Reverse(DataType list[], int from, int to){
+    for (int i = 0; i < to -from + 1; i++)
+    {
+        DataType temp = list[from + i];
+        list[from + i] = list[to - i];
+        list[to - i] = temp;
+    }
+    
+}
+void offsetP(DataType list[],int n, int p){
+    Reverse(list, 0, p - 1);
+    Reverse(list, p, n - 1);
+    Reverse(list, 0, n - 1);
+}
+
+
 int main()
 {
-    SeqList l;
-    l.data = new int[InitSize];
-    return 0;
 }
