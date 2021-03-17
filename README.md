dd6f7b3af61a4491caeb3100eadc87b53a2f946f
hekun1997 <48910648+hekun1997@users.noreply.github.com>
2021-03-17 17:04:18 +0800

    about List

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Data-Structures-And-Algorithms
+王道数据结构 数据结构代码
--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-typedef struct{
-    int *data;
-    int maxLength, length;
-} ist;
-
-
 int main()
 {
     /* 我的第一个 C 程序 */
